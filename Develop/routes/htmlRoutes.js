a5fcf0d2c1b41a9657e639bfe5e0a03eb953cae9
Movank1 <139717943+Movank1@users.noreply.github.com>
2023-11-19 19:48:03 -0500

    update

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,10 +1,10 @@
-
+// The express module is imported and assigned to the express variable.
 
 const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-// Define the HTML routes
+// the HTML routes are defined using the router.get() method
 router.get('/notes', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public/notes.html'));
 });
@@ -12,5 +12,7 @@ router.get('/notes', (req, res) => {
 router.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public/index.html'));
 });
+
+//  The router is exported using module.exports so that it can be used in other  application
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,11 @@
+//Import the express and api Routes and html Routes 
+
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-
-// Initialize the app and create a port
+// The app variable is initialized as an instance of the Express.js application
+// and create a port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
